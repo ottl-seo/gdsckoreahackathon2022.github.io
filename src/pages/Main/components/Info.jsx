3a809e17efbfc61f345732e46371c3d5fab72745
woodi <woodi.daily@gmail.com>
2022-01-14 16:54:45 +0900

    Add Main Page

---
+++ src/pages/Main/components/Info.jsx
@@ -0,0 +1,22 @@
+export const contestInfo = [
+  {
+    title: '행사시작',
+    content: '2022.02.04 5 p.m',
+  },
+  {
+    title: '해커톤주제',
+    content: 'UN17 中 (당일발표)',
+  },
+  {
+    title: '행사종료',
+    content: '2022.02.05 3 p.m',
+  },
+  {
+    title: '참가 팀',
+    content: '선착순 40팀',
+  },
+  {
+    title: '참가자격',
+    content: '팀장(GDSC member) 외 최대 3명',
+  },
+];
