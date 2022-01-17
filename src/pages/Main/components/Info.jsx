6fec3b1f7878da66964bb03feb62365e4d8d5cf3
Mayleaf <uh.jeong651@gmail.com>
2022-01-17 21:15:45 +0900

    feat: update info in home

--- src/pages/Main/components/Info.jsx
+++ src/pages/Main/components/Info.jsx
@@ -5,7 +5,7 @@ export const contestInfo = [
   },
   {
     title: '해커톤주제',
-    content: 'UN17 中 (당일발표)',
+    content: 'Quality Education, Gender Equality, Climate Action',
   },
   {
     title: '행사종료',
@@ -13,10 +13,15 @@ export const contestInfo = [
   },
   {
     title: '참가 팀',
-    content: '선착순 40팀',
+    content: '선착순 50팀',
   },
   {
     title: '참가자격',
     content: '팀장(GDSC member) 외 최대 3명',
   },
+  {},
+  {
+    title: '모집링크',
+    content: '19일 공개예정'
+  }
 ];
