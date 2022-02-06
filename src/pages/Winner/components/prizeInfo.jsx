f346d843b9cdb26f7501379e402b9dbfd885b85d
Mayleaf <uh.jeong651@gmail.com>
2022-02-06 12:31:20 +0900

    feat: add thumbnail

--- src/pages/Winner/components/prizeInfo.jsx
+++ src/pages/Winner/components/prizeInfo.jsx
@@ -1,91 +1,100 @@
-import MOCKUP from '../img/mockup.png';
+import CNB from '../img/07_CnB.png';
+import AREUMDEULGI from '../img/15_areum-deulgi.png';
+import HYOJASON from '../img/23_hyojason.jpeg';
+import GEDI from '../img/24_GeDi.png';
+import EASYPEASY from '../img/32_EasyPeasy.png';
+import GLITTER from '../img/33_Glitter.png';
+import GISTREE from '../img/35_GISTree.png';
+import HOMEWORK from '../img/36_Homework.gif';
+import GREENGREEN from '../img/38_GreenGreen.png';
+import GABOJAGO from '../img/42_Gabojago.png';
 
 export const TopTeamInfo = {
-  teamName: 'TeamName',
-  teamMembers: ['홍길동', '홍길동', '홍길동'],
-  projectThumbnail: MOCKUP,
-  githubRepo: 'https://github.com/',
+  teamName: 'CnB',
+  teamMembers: ['이준용', '정지윤', '한태규', '유성주'],
+  projectThumbnail: CNB,
+  githubRepo: 'https://github.com/gdsckoreahackathon2022/07_CnB',
   projectDescription:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.',
+    '지구를 생각하는 작은 마음들이 만들어가는 변화를 모토로, 작은 실천으로 지구의 아픔을 해결하고자 프로젝트를 진행하게 되었습니다. 티끌을 통해 과거와 현재, 근 미래의 기온을 차트를 통해 비교해 볼 수 있으며 환경 관련 뉴스를 손쉽게 확인할 수 있습니다. 미션을 클리어하며 직접 환경 보호를 실천할 수 있으며, 5개의 랜덤 퀴즈를 풀며 환경 지식을 쌓을 수도 있습니다. 미션과 퀴즈로 얻은 점수는 다른 유저들과 비교 가능합니다.',
 };
 
 export const GrandTeamInfo = [
   {
-    teamName: 'TeamName',
-    teamMembers: ['홍길동', '홍길동', '홍길동'],
-    projectThumbnail: MOCKUP,
-    githubRepo: 'https://github.com/',
+    teamName: '튀김조심',
+    teamMembers: ['심수연', '김소정', '김여원', '조용환'],
+    projectThumbnail: AREUMDEULGI,
+    githubRepo: 'https://github.com/gdsckoreahackathon2022/15_areum-deulgi',
     projectDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.',
+      "'아름들기'는 '아름답게 나이들기'를 줄인 말으로 나이든다는 것 자체가 아름다움을 강조하고자 하였습니다. 인생을 아름답게 살아가는 방법을 제시하는 서비스와 잘 어우러지는 이름이기에 선정하였습니다. 서비스는 시니어층이 관심을 가지는 분야인 요리, 운동, 음악, 생활의 네 가지 카테고리로 분류되어 있습니다. 각 카테고리별로 다양한 교육 서비스가 제공되며, 디지털 기기에 익숙하지 않은 시니어를 위해 직관적이고 간단한 UI를 제작하고자 노력했습니다.",
   },
   {
-    teamName: 'TeamName',
-    teamMembers: ['홍길동', '홍길동', '홍길동'],
-    projectThumbnail: MOCKUP,
-    githubRepo: 'https://github.com/',
+    teamName: '잔다르크',
+    teamMembers: ['오송경', '장지현', '장윤주'],
+    projectThumbnail: HYOJASON,
+    githubRepo: 'https://github.com/gdsckoreahackathon2022/23_hyojason',
     projectDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.',
+      '다양한 소비층을 겨냥한 애플리케이션이 많이 개발되고 있지만 노년층을 위한 애플리케이션의 부족을 느껴, 노년층이 사용하기 편리하기에 직관적이고 양질의 컨텐츠를 담을 애플리케이션을 개발하고자 하였습니다. 그 중에서도 노년층이 생활을 하는 데 있어서 꼭 필요한 정보지만 스마트폰 사용 미숙으로 인해 쉽게 접근하지 못하는 정보들에 집중했습니다. 실제 교육부 성인문해 학교 교과서에 기반하여 가정, 사회에서 필요한 정보들에 쉽게 습득하고 일반 시사, 생활정보, 복지정책정보, 커뮤니티 등 디지털 소외계층인 노년층의 양질의 교육을 위한 컨텐츠들로 구성하였습니다.',
   },
 ];
 
 export const ExcellentTeamInfo = [
   {
-    teamName: 'TeamName',
-    teamMembers: ['홍길동', '홍길동', '홍길동'],
-    projectThumbnail: MOCKUP,
-    githubRepo: 'https://github.com/',
+    teamName: '다다른',
+    teamMembers: ['최원혁', '강다인', '신형철', '김예은'],
+    projectThumbnail: GEDI,
+    githubRepo: 'https://github.com/gdsckoreahackathon2022/24_GeDi',
     projectDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.',
+      'GeDi는 일상 속에서 관습처럼 굳어진 채 사용되는 성차별 단어를 성평등 단어로 바꾸기 위해 기획된 성평등 단어사전입니다. 간단한 문제풀이를 통해 스스로의 성평등 의식을 점검해보고, 사용자가 보다 높은 성평등 의식 수준을 지향할 수 있도록 경험을 제공합니다. 또한 직접 구축한 성평등 단어사전을 통하여 잘 알지 못했던 성차별 단어들을 알고 스스로 언어습관을 바꾸어갈 수 있으며 나아가 AI 기술을 활용한 단어 인식을 통해 일상생활 속에서의 성차별 단어들을 성평등 단어사전에 구축한 단어로 바꾸어볼 수 있습니다.',
   },
   {
-    teamName: 'TeamName',
-    teamMembers: ['홍길동', '홍길동', '홍길동'],
-    projectThumbnail: MOCKUP,
-    githubRepo: 'https://github.com/',
+    teamName: 'AH-HA',
+    teamMembers: ['호선우', '김현아', '이소희', '강희원'],
+    projectThumbnail: EASYPEASY,
+    githubRepo: 'https://github.com/gdsckoreahackathon2022/32_EasyPeasy',
     projectDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.',
+      '다크 데이터는 저장하고 있으나 내용 또는 가치가 확인되지 않는 데이터를 말합니다. 베리타스에 따르면 다크데이터때문에 2020년에 약 580만톤의 이산화탄소가 배출되었고, 이 배출량은 자동차로 지구를 무려 575000바퀴 돌았을 때와 동일하다고 합니다. 손가락 하나만 까딱해서 다크데이터를 줄여 지구를 함께 지켜보시겠어요? 저희 AH-HA 팀은 우리의 일상 속에서 다크 데이터를 줄여 환경을 보호하고 이를 생활습관화할 수 있는 서비스, EasyPeasy를 제안합나다.',
   },
   {
-    teamName: 'TeamName',
-    teamMembers: ['홍길동', '홍길동', '홍길동'],
-    projectThumbnail: MOCKUP,
-    githubRepo: 'https://github.com/',
+    teamName: 'Glitter',
+    teamMembers: ['김진성', '남윤지', '박희준', '최우영'],
+    projectThumbnail: GLITTER,
+    githubRepo: 'https://github.com/gdsckoreahackathon2022/33_Glitter',
     projectDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.',
+      'BERT 기반 팀빌딩 서비스 구현으로  팀 구성단계에서의 리스크 감소와 협업 간 불협화음을 최소화해 스터디, 프로젝트 등의 그룹 활동을 증진시키고 최종적으로 양질의 교육을 실현하고자 합니다. 협업 능력이 강조됨에 따라 팀 프로젝트 형식의 수업이 이뤄지나 팀원 임의 구성 과정에서 비협조적인 인원( 소위 조별과제 빌런 )이 생겨났고 그에 따라 협업에 대한 부정적인 인식이 형성되고는 합니다. 이를 해결하고자 서로 비슷한 성향과 가치관을 가진 사람끼리 팀을 구성시키도록 지원하는 서비스를 고안하였습니다. Check Check은 서로 같은 질문에 대한 리더의 답과 지원자의 답을 비교해 일치율 분석, 리스크를 최소화한 팀빌딩을 가능케합니다.',
   },
 ];
 
 export const ParticipationTeamInfo = [
   {
-    teamName: 'TeamName',
-    teamMembers: ['홍길동', '홍길동', '홍길동'],
-    projectThumbnail: MOCKUP,
-    githubRepo: 'https://github.com/',
+    teamName: 'GISTree',
+    teamMembers: ['김철희', '이준명', '신동민'],
+    projectThumbnail: GISTREE,
+    githubRepo: 'https://github.com/gdsckoreahackathon2022/35_GISTree',
     projectDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.',
+      '지구를 위해 쓰레기도 돈 내고 버려야 하는 요즘 시대, 올바른 재활용을 위해 지스트리가 나섰다! 전세계적으로 쓰레기 매립, 재활용 등 쓰레기 처리 문제로 골머리를 앓고 있습니다. 길거리 쓰레기통이 늘어나는 추세이지만 길바닥에 돌아다니는 쓰레기는 여전히 늘어나고 있습니다. Trash Fresh는 주변에 가까운 쓰레기통의 위치를 알려주며 쓰레기 이미지 분석을 통한 재활용 가능여부를 판단합니다.',
   },
   {
-    teamName: 'TeamName',
-    teamMembers: ['홍길동', '홍길동', '홍길동'],
-    projectThumbnail: MOCKUP,
-    githubRepo: 'https://github.com/',
+    teamName: '홈워크',
+    teamMembers: ['이진아', '나정후', '이하은', '공혁준'],
+    projectThumbnail: HOMEWORK,
+    githubRepo: 'https://github.com/gdsckoreahackathon2022/36_Homework',
     projectDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.',
+      "‘성차별' 문제는 수년간 사회적으로 이슈가 되고 있습니다. 사회적으로 논란이 되는 성차별은 가족 내에서도 쉽게 찾아볼 수 있습니다. 한국보건사회연구원의 통계에 따르면 가사활동 전반에 걸쳐 약 70% 수준의 부인 집중 현상이 관찰되고 있다고 합니다. 이는 우리나라의 가부장주의 전통으로 인한 성 역할에 대한 고정관념 때문입니다. 성평등을 가로 막는 장애물, 성 역할! 이를 해결하기 위한 서비스 홈워크입니다.",
   },
   {
-    teamName: 'TeamName',
-    teamMembers: ['홍길동', '홍길동', '홍길동'],
-    projectThumbnail: MOCKUP,
-    githubRepo: 'https://github.com/',
+    teamName: '초록초록',
+    teamMembers: ['신지민', '김현균', '박현서', '유다빈'],
+    projectThumbnail: GREENGREEN,
+    githubRepo: 'https://github.com/gdsckoreahackathon2022/38_GreenGreen',
     projectDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.',
+      '길을 걷다보면 길가에 무수히 많은 쓰레기들을 마주칠 수 있습니다. 해외에서는 스웨덴어의 줍다(plocka up)와 영어단어 달리기(jogging)의 합성어인 ‘플로깅(plogging)’ 봉사활동인 줍깅으로 걷거나 뛰면서 길거리의 쓰레기를 줍는 활동을 많이 진행하고 있습니다. 초록초록은 길을 걸으면서 쓰레기를 줍고 주변 쓰레기통의 위치를 확인하여 처리할 수 있는 앱을 제작하여 길가의 쓰레기 근절과 코로나 시대로 인해 부족했던 운동을 실천함으로써 쓰레기도 줍고 보람도 얻는 일상속의 건강한 지구 지킴이가 될 수 있습니다.',
   },
   {
-    teamName: 'TeamName',
-    teamMembers: ['홍길동', '홍길동', '홍길동'],
-    projectThumbnail: MOCKUP,
-    githubRepo: 'https://github.com/',
+    teamName: '가보자고',
+    teamMembers: ['김보경', '조소연', '강정인', '김혜송'],
+    projectThumbnail: GABOJAGO,
+    githubRepo: 'https://github.com/gdsckoreahackathon2022/42_Gabojago',
     projectDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.',
+      '녹색탐지기는 Google mobile vision 을 이용해서 바코드 스캔을 통해 물건을 찍었을 때, 환경오염 부하나 폐기물 발생이 적은지 확인하는 어플리케이션입니다. 환경 문제가 심각해짐에 따라 환경파괴를 야기하는 화석연료 대신에 공해와 환경오염이 적은 대체 에너지원을 이용한 물품들의 생산이 증가하였습니다. 해당 제품들의 사용을 소비자에게 장려하기 위해 Google Mobile Vision 기술을 이용하여 어떠한 제품이 환경친화적인지를 소비자가 인식할 수 있도록 하는 서비스를 기획하게 되었습니다. 바코드 스캔을 통해 제품들의 정보를 획득하여 환경오염 부하나 폐기물 발생이 적은지를 파악한 후 환경친화도가 높은 제품의 사용을 장려하여 생활 쓰레기로 인한 환경오염을 감소시키는 것이 목적입니다.',
   },
 ];
